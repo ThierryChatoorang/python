6bec88628ebf9cce27c0a482f106b53c2efb62ee
ThierryChatoorang <181323867+ThierryChatoorang@users.noreply.github.com>
2026-02-04 13:10:49 +0100

    les1 student

---
+++ Python/opdracht 1/Les1_student.py
@@ -0,0 +1,23 @@
+
+
+# Een class is een blauwdruk/sjabloon voor het maken van objecten
+# Een object is een concrete instantie van die class met eigen geheugenadres
+
+class Student:
+    pass  # pass is een placeholder - de class is nu nog leeg, maar dat is oké
+
+
+# Maak het eerste Student object
+student1 = Student()
+print(student1)
+
+# Maak het tweede Student object
+student2 = Student()
+print(student2)
+
+# Verschil tussen class en object:
+# - Een class is de blauwdruk (bijv. een bouwtekening van een huis)
+# - Een object is een concrete versie van die blauwdruk (bijv. het echte huis dat gebouwd is)
+
+
+
